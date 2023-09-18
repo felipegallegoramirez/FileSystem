ceb725e747e74a23a6ae25ef6b72de4b3b3520b4
Felipe Gallego <felipegallego600@gmail.com>
2023-09-18 18:49:34 -0500

    bases backend

--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  //{ path: 'login', component: LoginRegisterComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, {useHash:true}),
+
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
