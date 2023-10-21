32ab18d30ff2a2f01bcbe0c35872a979e329ef47
Felipe Gallego <felipegallego600@gmail.com>
2023-10-21 18:02:09 -0500

    login

--- src/Frontend/src/app/components/login/login.component.ts
+++ src/Frontend/src/app/components/login/login.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  ngOnInit(): void {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+    // @ts-ignore: Object is possibly 'null'
+    
+    signUpButton.addEventListener('click', () => {
+      // @ts-ignore: Object is possibly 'null'
+      container.classList.add("right-panel-active");
+    });
+    // @ts-ignore: Object is possibly 'null'
+    signInButton.addEventListener('click', () => {
+      // @ts-ignore: Object is possibly 'null'
+      container.classList.remove("right-panel-active");
+    });
+
+  }
 
 }
