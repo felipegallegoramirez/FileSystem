5597567601fb1c75819858fabc06ec0912bfcd18
Felipe Gallego <felipegallego600@gmail.com>
2023-10-21 17:45:31 -0500

    aboutus

--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -2,26 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [
-  //{ path: 'login', component: LoginRegisterComponent },
-]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, {useHash:true}),
-
   ],
-  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
